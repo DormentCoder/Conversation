b4c37f63a28e76e1f7f1fbbc75d14dcf1569b3aa
DormentCoder <29092866@students.lincoln.ac.uk>
2024-12-05 14:00:16 +0000

    Add files via upload

---
+++ Conversation.py
@@ -0,0 +1,8 @@
+# Conversation by Dc117 Corp.
+
+print('Conversation V2.10')
+print('Hello')
+name_entered = input('What is your name? ')
+print('Thank you', name_entered)
+year = int(input('What year is it please? '))
+print('Ah, it is', year, 'thank you.')
